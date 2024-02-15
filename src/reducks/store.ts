7242657a97287340688a93d72f9e40367d303de2
tzptzptzptzptzp <git@tzp.pink>
2024-02-16 00:11:45 +0900

    🏷️ Storeの型をエクスポート

--- src/reducks/store.ts
+++ src/reducks/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
+export type RootState = ReturnType<typeof store.getState>;
+
 export const store = configureStore({
   reducer: rootReducer,
 });
