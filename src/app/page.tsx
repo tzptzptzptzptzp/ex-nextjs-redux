dde058387db35b3a2326f4f8feb9b88a219d9ce9
tzptzptzptzptzp <git@tzp.pink>
2024-02-16 00:16:24 +0900

    ✨ 更新ボタンを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ export default function Home() {
   return (
     <Provider store={store}>
       <main className="flex items-center justify-center w-screen h-screen">
-        <div className="p-8 shadow-lg">
+        <div className="flex flex-col gap-4 w-96 p-8 shadow-lg">
           <UserProfile />
+          <button className="w-4/5 mx-auto py-1 bg-blue-500 text-white rounded-md">
+            Update
+          </button>
         </div>
       </main>
     </Provider>
