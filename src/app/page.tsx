28e6c10cd7269471bc921eafb01e2ca3903d07b4
tzptzptzptzptzp <git@tzp.pink>
2024-02-16 00:12:00 +0900

    ✨ Providerでラップ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { Provider } from "react-redux";
+import { store } from "@/reducks/store";
+import { UserProfile } from "@/components/UserProfile/UserProfile";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-screen h-screen">
-      <div className="p-8 shadow-lg"></div>
-    </main>
+    <Provider store={store}>
+      <main className="flex items-center justify-center w-screen h-screen">
+        <div className="p-8 shadow-lg">
+          <UserProfile />
+        </div>
+      </main>
+    </Provider>
   );
 }
