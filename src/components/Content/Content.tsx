13f7ff990d1639afe93a877f783904496a9503ab
tzptzptzptzptzp <git@tzp.pink>
2024-02-23 19:37:45 +0900

    ⚡️ Redux管理下のカウントでデータを取得するように変更

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { AppDispatch } from "@/reducks/store";
+import { AppDispatch, RootState } from "@/reducks/store";
 import { getUser } from "@/reducks/user/operations";
 
 import { Counter } from "../Counter/Counter";
 import { UserProfile } from "../UserProfile/UserProfile";
 
 export const Content = () => {
-  const [count, setCount] = useState(1);
-
   const dispatch = useDispatch<AppDispatch>();
+  const count = useSelector((state: RootState) => state.count.count);
 
   const handleClick = () => {
     dispatch(getUser(count.toString())).catch((err) => console.error(err));
