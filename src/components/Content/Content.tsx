767f430a268e1fe6559da7947c08aa5d7661a147
tzptzptzptzptzp <git@tzp.pink>
2024-02-23 19:32:54 +0900

    ⚡️ Reduxでカウントを管理するように変更

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -19,7 +19,7 @@ export const Content = () => {
   return (
     <div className="flex flex-col gap-4 w-96 p-8 shadow-lg">
       <UserProfile />
-      <Counter count={count} setCount={setCount} />
+      <Counter />
       <button
         className="w-4/5 mx-auto py-1 bg-blue-500 text-white rounded-md"
         onClick={handleClick}
