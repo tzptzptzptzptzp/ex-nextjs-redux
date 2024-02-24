bd102e86b98dfcf045de3f409d4a651cf9d3b0ce
tzptzptzptzptzp <git@tzp.pink>
2024-02-24 23:31:32 +0900

    ⚡️ TemplateでProviderを使用するように変更

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "@/reducks/store";
+
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <Provider store={store}>
+      <div>{children}</div>
+    </Provider>
+  );
 }
