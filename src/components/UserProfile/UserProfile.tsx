c9c9cbe883f82a06ad8a034f212ed2da48d65421
tzptzptzptzptzp <git@tzp.pink>
2024-02-25 22:58:22 +0900

    ⚡️ ページに応じて表示するコンテンツを切り替える処理を追加

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,23 +1,32 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { useSelector } from "react-redux";
 import { RootState } from "@/reducks/store";
 
 export const UserProfile = () => {
   const user = useSelector((state: RootState) => state.user);
+  const path = usePathname();
   return (
     <div>
       <div className="flex gap-2">
         <h3>User Profile</h3>
-        <Link className=" hover:text-blue-400" href="/user/detail">
-          →
-        </Link>
+        {path === "/" && (
+          <Link className=" hover:text-blue-400" href="/user/detail">
+            →
+          </Link>
+        )}
       </div>
       <p>ID: {user.id}</p>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
       <p>City: {user.address.city}</p>
+      {path !== "/" && (
+        <div className="mt-4 text-center">
+          <Link href={"/"}>- HOME -</Link>
+        </div>
+      )}
     </div>
   );
 };
