e5ce453ec4de5bf384a47d980f01b87758f65f4c
tzptzptzptzptzp <git@tzp.pink>
2024-02-25 22:59:03 +0900

    ✨ 現在のStateで管理するユーザーを表示する処理を実装

--- src/app/user/detail/page.tsx
+++ src/app/user/detail/page.tsx
@@ -1,5 +1,9 @@
+import { UserProfile } from "@/components/UserProfile/UserProfile";
+
 export default function UserDetail() {
   return (
-    <main className="flex items-center justify-center w-screen h-screen"></main>
+    <main className="flex items-center justify-center w-screen h-screen">
+      <UserProfile />
+    </main>
   );
 }
