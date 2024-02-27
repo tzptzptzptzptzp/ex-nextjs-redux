836da3973dc9563f009d71fc7aa0bf406296187b
tzptzptzptzptzp <git@tzp.pink>
2024-02-27 22:41:41 +0900

    🌱 Userの初期値を更新

--- src/reducks/user/initializes.ts
+++ src/reducks/user/initializes.ts
@@ -1,18 +1,23 @@
 import { UserStateType } from "./types";
 
 export const initialUserState: UserStateType = {
-  id: 0,
-  name: "",
-  username: "",
-  email: "",
-  address: {
-    street: "",
-    suite: "",
-    city: "",
-    zipcode: "",
-    geo: {
-      lat: "",
-      lng: "",
+  user: {
+    id: 0,
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: "",
+        lng: "",
+      },
     },
   },
+  loading: false,
+  error: false,
+  errorMessage: "",
 };
