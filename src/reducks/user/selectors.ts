b486eb9c1740167ada2570ee497b542ea3f39f9e
tzptzptzptzptzp <git@tzp.pink>
2024-03-02 22:13:47 +0900

    🚨 Selectorのエラーを解消

--- src/reducks/user/selectors.ts
+++ src/reducks/user/selectors.ts
@@ -2,12 +2,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store";
 
 export const userSelector = () => {
-  const state = useSelector((state: RootState) => state);
+  const state = useSelector((state: RootState) => state.user);
 
   return {
-    user: state.user.user,
-    loading: state.user.loading,
-    error: state.user.error,
-    errorMessage: state.user.errorMessage,
+    user: state.user,
+    loading: state.loading,
+    error: state.error,
+    errorMessage: state.errorMessage,
   };
 };
